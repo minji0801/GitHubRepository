a85a7063e8909f0b3c4f6739b65c62851a609e3a
Minji Kim <minji_0801@naver.com>
2022-09-12 15:20:09 +0900

    Add Repository and configure RepositoryListCell's UI

--- GitHubRepository2/RepositoryListCell.swift
+++ GitHubRepository2/RepositoryListCell.swift
@@ -9,7 +9,7 @@ import UIKit
 import SnapKit
 
 class RepositoryListCell: UITableViewCell {
-  var repository: String?
+  var repository: Repository?
   
   let nameLabel = UILabel()
   let descriptionLabel = UILabel()
@@ -27,6 +27,49 @@ class RepositoryListCell: UITableViewCell {
       contentView.addSubview($0)
     }
     
+    guard let repository = repository else { return }
+
+    nameLabel.text = repository.name
+    nameLabel.font = .systemFont(ofSize: 15, weight: .bold)
     
+    descriptionLabel.text = repository.description
+    descriptionLabel.font = .systemFont(ofSize: 15)
+    descriptionLabel.numberOfLines = 2
+    
+    starImageView.image = UIImage(systemName: "star")
+
+    starLabel.text = "\(repository.stargazersCount)"
+    starLabel.font = .systemFont(ofSize: 16)
+    starLabel.textColor = .gray
+    
+    languageLabel.text = repository.language
+    languageLabel.font = .systemFont(ofSize: 16)
+    languageLabel.textColor = .gray
+    
+    nameLabel.snp.makeConstraints { make in
+      make.top.leading.trailing.equalToSuperview().inset(18)
+    }
+    
+    descriptionLabel.snp.makeConstraints { make in
+      make.top.equalTo(nameLabel.snp.bottom).offset(3)
+      make.leading.trailing.equalTo(nameLabel)
+    }
+    
+    starImageView.snp.makeConstraints { make in
+      make.top.equalTo(descriptionLabel.snp.bottom).offset(8)
+      make.leading.equalTo(descriptionLabel)
+      make.width.height.equalTo(20)
+      make.bottom.equalToSuperview().inset(18)
+    }
+    
+    starLabel.snp.makeConstraints { make in
+      make.centerY.equalTo(starImageView)
+      make.leading.equalTo(starImageView.snp.trailing).offset(5)
+    }
+    
+    languageLabel.snp.makeConstraints { make in
+      make.centerY.equalTo(starLabel)
+      make.leading.equalTo(starLabel.snp.trailing).offset(12)
+    }
   }
 }
